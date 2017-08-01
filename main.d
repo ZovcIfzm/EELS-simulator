2bd0d708da13e8fa07d04d4d1830aad61d03e2ac
Ajusa Jain <arhamjain@gmail.com>
2017-08-01 15:30:03 -0400

    basic class

--- main.d
+++ main.d
@@ -1,6 +1,24 @@
-import std.stdio, std.array, std.algorithm;
-
+import std.stdio, std.array, std.algorithm, std.conv;
+class PhaseSpace
+{
+	double data[][];
+    this(double v, double pos){
+    	data ~= [v];
+    	data ~= [pos];
+	}
+	void currentPhaseSpace(){
+		writeln(text(data));
+	}
+	void goForward(double dt){ //just here temporarily
+		foreach (ref items; data) { // overwrite value
+    		foreach (ref item; items)  item *= dt;
+		}
+	}
+}
 void main()
 {
-    //add code here
+	auto space = new PhaseSpace(2.001, 5.009); //Random numbers
+	space.currentPhaseSpace();
+    space.goForward(4);
+    space.currentPhaseSpace();
 }
