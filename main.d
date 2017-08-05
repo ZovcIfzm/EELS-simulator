75b7863fc81bcb0c39e4944b14549876fb14f5fe
ZovcIfzm <alexruan2001@gmail.com>
2017-08-04 23:47:45 -0400

    developed PSE methods
    Phase Space Expansion methods developed:
    -Free expansion method developed
    -Optical manipulation method renamed from reverseChirp
    (More accurate to actual occurance)

--- main.d
+++ main.d
@@ -1,6 +1,7 @@
 import std.stdio, std.array, std.algorithm, std.conv, std.math;
 class PhaseSpace{
-	double sigma, zeta, pos, v, a, b; //Can sigma and zeta be streamlined?
+	double sigmaZ, zetaZ, pos, a;
+	double sigmaVz, zetaVz, Vz, b, tau;
 	double[][] data;
     this(double v, double pos){
     	data ~= [v];
@@ -9,25 +10,32 @@ class PhaseSpace{
 	void currentPhaseSpace(){
 		writeln(text(data));
 	}
-		//Phase Space Evolution//
-//1
 	void goForward(double dt){ //just here temporarily
 		foreach (ref items; data) { // overwrite value
     		foreach (ref item; items)  item *= dt;
 		}
 	}
-//2
-	void reverseChirpA(double change){//a is a value, not 1/-1 shown in equation 1 on the sheet
+	void freeExpansion(){//To deal with processing we might need to make our own math functions. (less/more digits of accuracy)
+		this.zetaZ = sqrt(1/((1/pow(sigmaVz,2))+pow((b/tau),2)));
+		this.a = b*pow(zetaZ,2)/pow(tau,2);
+		this.sigmaZ = sqrt(1/((1/pow(tau,2))-pow(a/zetaZ,2)));
+	}
+	void opticalManipulationA(double change){
 		this.a = a + change;
 	}
-	void reverseChirpB(double change){//Is there a way to make one method but able to change both a and b for the specific object?
+	void opticalMantipulationB(double change){//Is there a way to make one method but able to change both a and b for the specific object?
 		this.b = b + change;
 	}
-//Conservation Checking
-	bool checkAreaConservation(double sigma, double zeta, double pos, double v, double a){	//Form A and B use different variables for sigma, zeta, and a.
-		//double consAValue = exp(-pow(-a,2)/(2*(pow(zeta,2))))/(2*PI*pow(sigma*zeta,2));		//Need to figure out how to use pos and v (Z and Vz) this equation sets them to 0)
-		double consAValue = exp((pow(zeta,2)/(-2*pow(sigma,2)))-pow(v-a,2)/(2*(pow(zeta,2))))/(2*PI*pow(sigma*zeta,2));	//Form A and B use different variables for sigma, zeta, and a. 
-		if(consAValue > 0.9 && consAValue < 1.1){//How much range will need to be decided, in reality the value actually goes above 1!
+/*Conservation Checking
+	Form A and B use different variables for sigma, zeta, and a.
+	consAValue needs to be integrated! 
+	This process only needs to be run as a sort of debug tool for the programmer- the program might not necessarily need it
+	-Should we have auto checkAreaConservation? 
+	-(More processing would be needed but as different variables are inputed the user can be warned if the program doesn't work and then it can be fixed)
+*/
+	bool checkAreaConservation(double sigma, double zeta, double pos, double v, double a){
+		double consAValue = exp((pow(zeta,2)/(-2*pow(sigma,2)))-pow(v-a,2)/(2*(pow(zeta,2))))/(2*PI*pow(sigma*zeta,2));	//needs to be integrated
+		if(consAValue > 0.9 && consAValue < 1.1){//randomly decided range to account for integration error 
 			writeln("Area is conserved");
 			return true;
 		}
@@ -36,7 +44,7 @@ class PhaseSpace{
 			writeln("Area:", consAValue);
 			return false;
 		}
-	bool checkVolumeConservation(){//Two checkAreaConservations but with different variables for the 1st and 2nd axis.
+	bool checkVolumeConservation(){//Two checkAreaConservations but with different variables for form A and form B
 		/*if(checkAreaConservation() &&checkAreaConservation()){
 			writeln("Volume is conserved");*/
 			return true;
@@ -47,7 +55,7 @@ class PhaseSpace{
 void main(){
 	auto space = new PhaseSpace(2.001, 5.009); //Random numbers
 	space.currentPhaseSpace();
-    space.goForward(4);
+    //space.goForward(4);
     space.currentPhaseSpace();
 	space.checkAreaConservation(1,1,1,1,1);
 	writeln("End of Program, enter anything to continue");
