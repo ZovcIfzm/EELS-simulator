557042fd5cf6c9a99d35511ee407146189b32bce
Alex Ruan <alexruan2222@gmail.com>
2021-12-07 21:16:56 -0500

    completed evolutionWithInteraction

--- PyMPI/main.py
+++ PyMPI/main.py
@@ -49,84 +49,9 @@ if rank < k.SPLIT_NUM:
     initPulse = k.INIT_PS
     pulses = ps.split(initPulse, startSplit, numSplits, k.PULSE_ENERGY, pool)
 
-    # assign space values for NN to sharedMemory
-    for i in range(numSplits):
-        sharedMem[(startSplit+i)*3] = pulses["pulseEnergy"].iloc[i]
-        sharedMem[(startSplit+i)*3+1] = pulses["VzC"].iloc[i]
-        sharedMem[(startSplit+i)*3+2] = pulses["zC"].iloc[i]
-    comm.Barrier()
+    ps.evolutionWithInteraction(
+        sharedMem, pulses, startSplit, numSplits, splitSize, 50)
 
-    # create window for one-way communication (other phase-space parameters)
-    oneWayBuf = pulses.to_numpy()
-    oneWayBufRecv = np.zeros(18*numSplits, dtype='d')
-    print(oneWayBuf.shape)
-    oneWayWin = MPI.Win.Create(oneWayBuf, 1, MPI.INFO_NULL, MPI.COMM_WORLD)
-    oneWayWin.Lock(rank)
-    oneWayWin.Put(oneWayBuf, target_rank=rank)
-    oneWayWin.Unlock(rank)
-
-    # find nearest neighbors
-    # convert values to dataframe, run parallel operations
-    df = pd.DataFrame(sharedMem.reshape((k.SPLIT_NUM, 3)),
-                      columns=["pulseEnergy", "VzC", "zC"])
-
-    neighbors = {}
-
-    for i in range(startSplit, startSplit+numSplits):
-        limitDf = pd.DataFrame(df["pulseEnergy"]*df["pulseEnergy"].iloc[i] /
-                               (pow(df["VzC"]-df["VzC"].iloc[i], 2) +
-                                pow(df["zC"]-df["zC"].iloc[i], 2)), columns=["limit"])
-        limitDf = limitDf[~limitDf.isin([np.nan, np.inf, -np.inf]).any(1)]
-        limitDf = limitDf[limitDf["limit"] > k.NN_LIMIT]
-        neighbors[i] = limitDf.index.tolist()
-
-    comm.Barrier()
-    print(neighbors)
-
-    # Retrive neighbor's values
-    neighborSpaces = {}
-    for key, value in neighbors.items():
-        for val in value:
-            neighborSpaces[val] = []
-
-    # Find corresponding ranks for neighbors
-    neighborRanks = {}
-    for key in neighborSpaces.keys():
-        targetRank = key//splitSize
-        if targetRank in neighborRanks:
-            neighborRanks[targetRank].append(key)
-        else:
-            neighborRanks[targetRank] = [key]
-
-    # Retrieve neighbors from ranks
-    for key, val in neighborRanks.items():
-        oneWayWin.Lock(key)
-        oneWayWin.Get(oneWayBufRecv, target_rank=key)
-        oneWayWin.Unlock(key)
-        spaceMatrix = oneWayBufRecv.reshape((-1, 18))
-        for ind in val:
-            neighborSpaces[ind] = spaceMatrix[ind % splitSize, :]
-
-    # Next step: calculate force effect on location
-
-    forces = [np.sum([integration.getForce(
-        pd.Series(j, index=k.COLUMNS), pulses.iloc[i-startSplit]["zC"], pulses.iloc[i-startSplit]["VzC"], pulses.iloc[i-startSplit]["pulseEnergy"]) for j in neighbors[i]], axis=0) for i in range(startSplit, startSplit+numSplits)]
-    print(forces)
-    '''
-    for i in range(startSplit, startSplit+numSplits):
-        for j in neighbors[i]:
-            neighbor = pd.Series(j, index=k.COLUMNS)
-            forceDf = integration.getForce(
-                neighbor, pulses.iloc[i-startSplit]["zC"], pulses.iloc[i-startSplit]["VzC"], pulses.iloc[i-startSplit]["pulseEnergy"])
-                '''
-    '''
-            df["pulseEnergy"]*df["pulseEnergy"].iloc[i] /
-                               (pow(df["VzC"]-df["VzC"].iloc[i], 2) +
-                                pow(df["zC"]-df["zC"].iloc[i], 2)), columns=["limit"])
-        limitDf = limitDf[~limitDf.isin([np.nan, np.inf, -np.inf]).any(1)]
-        limitDf = limitDf[limitDf["limit"] > k.NN_LIMIT]
-        neighbors[i] = limitDf.index.tolist()
-        '''
     '''
     # map with space-filling curve
     p, n = 10, 2
--- PyMPI/phase_space.py
+++ PyMPI/phase_space.py
@@ -1,3 +1,4 @@
+from mpi4py import MPI
 import math
 import pandas as pd
 import numpy as np
@@ -68,3 +69,82 @@ def split(s, startSplit, numSplits, pulseEnergy, pool):
 
     splitSpaces = pd.DataFrame(splitSpacesList, columns=k.COLUMNS)
     return splitSpaces
+
+
+def evolutionWithInteraction(sharedMem, pulses, startSplit, numSplits, splitSize, time):
+    # reiniitalize MPI to reduce method arguments
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # assign space values for NN to sharedMemory
+    for i in range(numSplits):
+        sharedMem[(startSplit+i)*3] = pulses["pulseEnergy"].iloc[i]
+        sharedMem[(startSplit+i)*3+1] = pulses["VzC"].iloc[i]
+        sharedMem[(startSplit+i)*3+2] = pulses["zC"].iloc[i]
+    comm.Barrier()
+
+    # create window for one-way communication (other phase-space parameters)
+    oneWayBuf = pulses.to_numpy()
+    oneWayBufRecv = np.zeros(18*numSplits, dtype='d')
+    oneWayWin = MPI.Win.Create(oneWayBuf, 1, MPI.INFO_NULL, MPI.COMM_WORLD)
+    oneWayWin.Lock(rank)
+    oneWayWin.Put(oneWayBuf, target_rank=rank)
+    oneWayWin.Unlock(rank)
+
+    # find nearest neighbors
+    # convert values to dataframe, run parallel operations
+    df = pd.DataFrame(sharedMem.reshape((k.SPLIT_NUM, 3)),
+                      columns=["pulseEnergy", "VzC", "zC"])
+
+    neighbors = {}
+
+    for i in range(startSplit, startSplit+numSplits):
+        limitDf = pd.DataFrame(df["pulseEnergy"]*df["pulseEnergy"].iloc[i] /
+                               (pow(df["VzC"]-df["VzC"].iloc[i], 2) +
+                                pow(df["zC"]-df["zC"].iloc[i], 2)), columns=["limit"])
+        limitDf = limitDf[~limitDf.isin([np.nan, np.inf, -np.inf]).any(1)]
+        limitDf = limitDf[limitDf["limit"] > k.NN_LIMIT]
+        neighbors[i] = limitDf.index.tolist()
+
+    comm.Barrier()
+    print(neighbors)
+
+    # Retrive neighbor's values
+    neighborSpaces = {}
+    for key, value in neighbors.items():
+        for val in value:
+            neighborSpaces[val] = []
+
+    # Find corresponding ranks for neighbors
+    neighborRanks = {}
+    for key in neighborSpaces.keys():
+        targetRank = key//splitSize
+        if targetRank in neighborRanks:
+            neighborRanks[targetRank].append(key)
+        else:
+            neighborRanks[targetRank] = [key]
+
+    # Retrieve neighbors from ranks
+    for key, val in neighborRanks.items():
+        oneWayWin.Lock(key)
+        oneWayWin.Get(oneWayBufRecv, target_rank=key)
+        oneWayWin.Unlock(key)
+        spaceMatrix = oneWayBufRecv.reshape((-1, 18))
+        for ind in val:
+            neighborSpaces[ind] = spaceMatrix[ind % splitSize, :]
+
+    # Next step: calculate force effect on location
+    forces = [np.sum([integration.getForce(
+        pd.Series(j, index=k.COLUMNS), pulses.iloc[i-startSplit]["zC"], pulses.iloc[i-startSplit]["VzC"], pulses.iloc[i-startSplit]["pulseEnergy"]) for j in neighbors[i]], axis=0) for i in range(startSplit, startSplit+numSplits)]
+
+    forces = pd.DataFrame([force if type(force) is np.ndarray else np.array([0.0, 0.0])
+                           for force in forces], columns=["zC", "VzC"])
+    if rank == 1:
+        print(pulses[["zC", "VzC"]])
+
+    pulses["zC"] += forces["zC"]*time/1E15
+    pulses["VzC"] += forces["VzC"]*time/1E15
+
+    if rank == 1:
+        print(pulses[["zC", "VzC"]])
